e1bad4ea304e00b7615f10b7a6d5cb0c0c88d699
Eugene V <jekir87@gmail.com>
2018-10-26 22:10:42 -0700

    Created HelperBase class

--- AddressBookTests/src/test/java/addressbook/tests/GroupCreateTests.java
+++ AddressBookTests/src/test/java/addressbook/tests/GroupCreateTests.java
@@ -7,11 +7,11 @@ public class GroupCreateTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
-        app.goToGroupPage();
-        app.initGroupCreation();
-        app.fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
-        app.submitGroupCreation();
-        app.returnToGroupPage();
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
     }
 
 }
